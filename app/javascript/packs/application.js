3c77ec39237fb1fd1fccf1b148ffae943f900d74
Germain Loret <germain.loret@gmail.com>
2019-01-04 12:23:26 +0100

    Update new form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { previewImageOnFileSelect } from '../components/photo_preview.js';
 
 initMapbox();
+previewImageOnFileSelect();
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,5 @@
 class Flat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  mount_uploader :photo, PhotoUploader
 end
